1f2c6e66c5131d13ee5ac300b1acec50ea342d37
Louis Grante <louis.grante@epitech.eu>
2021-05-17 20:12:30 +0200

    [UPDATE]: change host addr

--- OptiOffice.py
+++ OptiOffice.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello World!"
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
 
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    app.run(host='0.0.0.0', port=5000)
